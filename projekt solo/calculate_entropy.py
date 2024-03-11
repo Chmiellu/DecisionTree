c46c3d5fde29243f608a43f1a19df709d5dd7a9c
Chmiellu <tomasz.chmiel2@edu.uekat.pl>
2024-03-11 07:29:05 +0100

    works fine

--- projekt solo/calculate_entropy.py
+++ projekt solo/calculate_entropy.py
@@ -1,17 +1,65 @@
+import math
 from read_file import data
-def calculate_entropy(dataset):
-    total_count = len(data)
-    label_counts = {}
-    for row in data:
-        label = row[-1]  # Assuming the last column is the decision attribute
-        label_counts[label] = label_counts.get(label, 0) + 1
 
-    entropy = 0.0
-    for label in label_counts:
-        probability = label_counts[label] / total_count
+def calculate_entropy(dataset):
+    decision_attribute_counts = {}
+    total_instances = len(dataset)
+    for instance in dataset:
+        decision_attribute = instance[-1]
+        decision_attribute_counts[decision_attribute] = decision_attribute_counts.get(decision_attribute, 0) + 1
+    probabilities = {}
+    for decision_attribute, count in decision_attribute_counts.items():
+        probability = count / total_instances
+        probabilities[decision_attribute] = probability
+
+    entropy = 0
+    for probability in probabilities.values():
         if probability != 0:
-            entropy -= probability * (probability.bit_length() - total_count.bit_length())
+            entropy -= probability * math.log2(probability)
 
     return entropy
 
-print(calculate_entropy(data))
+def conditional_attribute_entropy(dataset):
+    num_attributes = len(dataset[0]) - 1
+    total_instances = len(dataset)
+    entropies = {}
+    for col in range(num_attributes):
+        attribute_counts = {}
+        decision_counts = {}
+        for instance in dataset:
+            attribute_value = instance[col]
+            decision_value = instance[-1]
+            if attribute_value not in decision_counts:
+                decision_counts[attribute_value] = {}
+            decision_counts[attribute_value][decision_value] = decision_counts[attribute_value].get(decision_value,0) + 1
+            attribute_counts[attribute_value] = attribute_counts.get(attribute_value, 0) + 1
+        attribute_entropy = 0
+        probabilities = {}
+        for attribute_value, count in attribute_counts.items():
+            probability = count / total_instances
+            probabilities[attribute_value] = probability
+            decision_values = decision_counts[attribute_value]
+            decision_value_counts = sum(decision_values.values())
+            decision_value_probabilities = {key: value / decision_value_counts for key, value in decision_values.items()}
+
+            for decision_probability in decision_value_probabilities.values():
+                    attribute_entropy -= probability * decision_probability * math.log2(decision_probability)
+
+
+        entropies[f'Attribute_{col + 1}'] = attribute_entropy  # Używamy zmienionej nazwy zmiennej
+
+    return entropies
+
+def print_results(entropy, attribute_entropies):
+    print(f"Entropia zbioru: {entropy}")
+    for attribute, attribute_entropy in attribute_entropies.items():
+        gain = entropy - attribute_entropy
+        gain_ratio = 0
+        print(f"\nInfo({attribute},T) = {attribute_entropy}")
+        print(f"Gain({attribute},T) = {gain}")
+        print(f"GainRatio({attribute},T) = {gain_ratio}")
+
+# Przykładowe użycie funkcji
+entropy = calculate_entropy(data)
+attribute_entropies = conditional_attribute_entropy(data)
+print_results(entropy, attribute_entropies)
--- projekt solo/read_file.py
+++ projekt solo/read_file.py
@@ -1,53 +1,54 @@
-file_path = r'C:\Users\tomek\OneDrive\Dokumenty\MAGISTERSKIE\2sem\Systemy uczące się\cwiczenia\projekt solo\dane testowe\testowaTabDec.txt'
+file_path = 'testowaTabDec.txt'
 
-with open(file_path, 'r') as file:
+with open(file_path) as file:
     data = [line.strip().split(',') for line in file]
 
 column_labels = [f'atr{i+1}' if i < len(data[0])-1 else 'atr decyzyjny' for i in range(len(data[0]))]
 
-print("Wybierz jedną z poniższych opcji:")
-print("1. Wyświetl tabelę z danymi.")
-print("2. Pokaż liczbę możliwych wartości dla każdego atrybutu.")
-print("3. Pokaż wystąpienia każdej wartości dla każdego atrybutu.")
+if __name__ == "__main__":
+    print("Wybierz jedną z poniższych opcji:")
+    print("1. Wyświetl tabelę z danymi.")
+    print("2. Pokaż liczbę możliwych wartości dla każdego atrybutu.")
+    print("3. Pokaż wystąpienia każdej wartości dla każdego atrybutu.")
 
-choice = input("Wpisz numer opcji: ")
+    choice = input("Wpisz numer opcji: ")
 
-if choice == '1':
-    custom_index_labels = [f'x{i+1}' for i in range(len(data))]
-    data_with_custom_index = dict(zip(custom_index_labels, data))
-    print(data_with_custom_index)
+    if choice == '1':
+        custom_index_labels = [f'x{i+1}' for i in range(len(data))]
+        data_with_custom_index = dict(zip(custom_index_labels, data))
+        print(data_with_custom_index)
 
-elif choice == '2':
-    num_possible_values = {}
-    for i, column in enumerate(column_labels):
-        unique_values = set(row[i] for row in data)
-        num_unique_values = len(unique_values)
-        num_possible_values[column] = num_unique_values
-    print("Liczba możliwych wartości dla każdego atrybutu:")
-    for column, count in num_possible_values.items():
-        print(f"{column}: {count}")
+    elif choice == '2':
+        num_possible_values = {}
+        for i, column in enumerate(column_labels):
+            unique_values = set(row[i] for row in data)
+            num_unique_values = len(unique_values)
+            num_possible_values[column] = num_unique_values
+        print("Liczba możliwych wartości dla każdego atrybutu:")
+        for column, count in num_possible_values.items():
+            print(f"{column}: {count}")
 
-    check_atr = 'atr3'
-    if check_atr in num_possible_values:
-        print(f"Liczba możliwych wartości dla atrybutu '{check_atr}' = {num_possible_values[check_atr]}")
+        check_atr = 'atr3'
+        if check_atr in num_possible_values:
+            print(f"Liczba możliwych wartości dla atrybutu '{check_atr}' = {num_possible_values[check_atr]}")
 
-elif choice == '3':
-    value_counts = {}
-    for i, column in enumerate(column_labels):
-        col_value_counts = {}
-        for row in data:
-            col_value_counts[row[i]] = col_value_counts.get(row[i], 0) + 1
-        value_counts[column] = col_value_counts
-    print("Wystąpienia każdej wartości dla każdego atrybutu:")
-    for column, col_value_counts in value_counts.items():
-        print(column)
-        for value, count in col_value_counts.items():
-            print(f"{value}: {count}")
+    elif choice == '3':
+        value_counts = {}
+        for i, column in enumerate(column_labels):
+            col_value_counts = {}
+            for row in data:
+                col_value_counts[row[i]] = col_value_counts.get(row[i], 0) + 1
+            value_counts[column] = col_value_counts
+        print("Wystąpienia każdej wartości dla każdego atrybutu:")
+        for column, col_value_counts in value_counts.items():
+            print(column)
+            for value, count in col_value_counts.items():
+                print(f"    {value}: {count}")
 
-    check_count = 'atr decyzyjny'
-    if check_count in value_counts:
-        exact_value_count = sum(1 for row in data if row[-1] == '1')
-        print(f"Ilość wystąpień wartości 1 dla atrybutu decyzyjnego '{check_count}': {exact_value_count}")
+        check_count = 'atr decyzyjny'
+        if check_count in value_counts:
+            exact_value_count = sum(1 for row in data if row[-1] == '1')
+            print(f"Ilość wystąpień wartości 1 dla atrybutu decyzyjnego '{check_count}': {exact_value_count}")
 
-else:
-    print("Nieprawidłowy wybór.")
+    else:
+        print("Nieprawidłowy wybór.")
